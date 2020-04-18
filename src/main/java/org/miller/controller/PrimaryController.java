c233a43860a263e48ff770804e8f89a537640891
Oleg Melnyk <omelnyk@MacBook-Pro-Oleg.local>
2020-04-19 00:53:00 +0300

    Added graph displaying on panel

--- src/main/java/org/miller/controller/PrimaryController.java
+++ src/main/java/org/miller/controller/PrimaryController.java
@@ -1,12 +1,38 @@
 package org.miller.controller;
 
+import com.brunomnsilva.smartgraph.graph.Digraph;
+import com.brunomnsilva.smartgraph.graph.DigraphEdgeList;
+import com.brunomnsilva.smartgraph.graphview.SmartCircularSortedPlacementStrategy;
+import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
+import com.brunomnsilva.smartgraph.graphview.SmartGraphProperties;
+import com.brunomnsilva.smartgraph.graphview.SmartPlacementStrategy;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.AnchorPane;
 
 public class PrimaryController {
 
+  private boolean isGraphInit = false;
+  private SmartGraphPanel<String, String> graphView;
+  private Digraph<String, String> graph = new DigraphEdgeList<>();
   private String elementsSchemaEquation;
+  @FXML
+  private AnchorPane graphContainer;
+
+  @FXML
+  public void initialize() {
+
+    SmartPlacementStrategy strategy = new SmartCircularSortedPlacementStrategy();
+    SmartGraphProperties smartGraphProperties = new SmartGraphProperties(getClass().getClassLoader().getResourceAsStream("smartgraph.properties"));
+    graphView = new SmartGraphPanel<>(graph, smartGraphProperties, strategy);
+
+    graphView.setPrefHeight(600);
+    graphView.setPrefWidth(1000);
+    graphView.getStylesheets().add(getClass().getClassLoader().getResource("smartgraph.css").toExternalForm());
+
+    graphContainer.getChildren().add(graphView);
+  }
 
   @FXML
   public void openAddSchemaModal(ActionEvent actionEvent) {
@@ -19,5 +45,36 @@ public class PrimaryController {
         + "For example: (E1 | E2) & E3 & E4 - group of parallel E1 and E2 in sequence with E3 and E4.");
 
     dialog.showAndWait().ifPresent(name -> this.elementsSchemaEquation = name);
+
+    graph.vertices().forEach(graph::removeVertex);
+    graph.edges().forEach(graph::removeEdge);
+
+    graph.insertVertex("1");
+    graph.insertVertex("2");
+    graph.insertVertex("3");
+    graph.insertVertex("4");
+    graph.insertVertex("5");
+    graph.insertVertex("6");
+    graph.insertVertex("7");
+
+    graph.insertEdge("1", "2", "λ1");
+    graph.insertEdge("1", "3", "λ2");
+    graph.insertEdge("1", "4", "λ3");
+
+    graph.insertEdge("3", "5", "λ4");
+    graph.insertEdge("3", "6", "λ5");
+
+    graph.insertEdge("4", "7", "λ6");
+
+    if(!isGraphInit){
+
+      graphView.init();
+      graphView.update();
+      isGraphInit = true;
+    }else {
+      graphView.update();
+    }
+
+    graphView.update();
   }
 }
